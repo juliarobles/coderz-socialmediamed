9ced60e403520926eae26799f6205714fe5dde4b
danim <danim@LAPTOP-47OFHLN3.home>
2019-11-24 00:08:46 +0100

    Editar perfil

---
+++ IngReqSocialMediaMed/src/vista/EditarPerfil.java
@@ -0,0 +1,56 @@
+package vista;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import java.awt.Toolkit;
+
+public class EditarPerfil {
+
+	private JFrame frameEditarPerfil;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					EditarPerfil window = new EditarPerfil();
+					window.frameEditarPerfil.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public EditarPerfil() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		int width = 800;
+		int height = 500;
+		frameEditarPerfil = new JFrame();
+		frameEditarPerfil.setTitle("AccionSocialMed - Editar perfil");
+		frameEditarPerfil.setIconImage(Toolkit.getDefaultToolkit().getImage(EditarPerfil.class.getResource("/resources/_Logo AccionSocialMed png.png")));
+		frameEditarPerfil.setResizable(false);
+		frameEditarPerfil.setBounds(100, 100, width, height);
+		frameEditarPerfil.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frameEditarPerfil.getContentPane().setLayout(null);
+		
+		JLabel lblHola = new JLabel("Hola");
+		lblHola.setBounds(179, 120, 49, 14);
+		frameEditarPerfil.getContentPane().add(lblHola);
+	}
+}
